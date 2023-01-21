71fe9a442587cfe7b928db183d677d728247cbf6
StoicInsight <gettantolcen@gmail.com>
2023-01-21 00:35:10 -0500

    test

--- Three-Book/public/client.js
+++ Three-Book/public/client.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
-import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
+import { OrbitControls } from 'threeLibs/controls/OrbitControls'
+import Stats from 'threeLibs/jsm/libs/stats.module'
+import { GUI } from 'threeLibs/jsm/libs/dat.gui.module'
 
 const scene = new THREE.Scene()
 
